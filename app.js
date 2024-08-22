605223361a4e87ae3ee611b92aebe354a46fbda7
Muhammad Tariq <pay10473@gmail.com>
2024-08-22 10:52:57 -0400

    middle commitment

--- app.js
+++ app.js
@@ -1,1 +1,87 @@
-fgdfgdf
+const nav1 = document.querySelector("#menu-1-button")
+const nav2 = document.querySelector("#menu-2-button")
+const nav3 =document.querySelector("#menu-3-button")
+const nav4 =document.querySelector("#menu-4-button")
+const nav5 = document.querySelector("#menu-5-button")
+const nav6 = document.querySelector("#menu-6-button")
+const nav7 = document.querySelector("#menu-7-button")
+const menuOne = document.querySelector("#menu-1")
+const menuTwo = document.querySelector("#menu-2")
+const menuThree = document.querySelector("#menu-3")
+const menuFour = document.querySelector("#menu-4")
+const menuFive = document.querySelector("#menu-5")
+const menuSix = document.querySelector("#menu-6")
+const menuSeven = document.querySelector("#menu-7")
+const menuOneShow = ()=> {
+menuOne.classList.toggle("show")
+menuTwo.classList.remove("show")
+menuThree.classList.remove("show")
+menuFour.classList.remove("show")
+menuFive.classList.remove("show")
+menuSix.classList.remove("show")}  
+menuSeven.classList.remove("show")  
+nav1.addEventListener("click", menuOneShow)
+
+const menuTwoShow = ()=> {
+menuOne.classList.remove("show")
+menuTwo.classList.toggle("show")
+menuThree.classList.remove("show")
+menuFour.classList.remove("show")}
+menuFive.classList.remove("show")
+menuSix.classList.remove("show")
+menuSeven.classList.remove("show")
+nav2.addEventListener("click", menuTwoShow)
+
+const menuThreeShow = ()=> {
+    menuOne.classList.remove("show")
+    menuTwo.classList.remove("show")
+    menuThree.classList.toggle("show")
+    menuFour.classList.remove("show")
+    menuFive.classList.remove("show")
+    menuSix.classList.remove("show")
+    menuSeven.classList.remove("show")}
+    nav3.addEventListener("click", menuThreeShow)
+
+    const menuFourShow = ()=> {
+            menuOne.classList.remove("show")
+            menuTwo.classList.remove("show")
+            menuThree.classList.remove("show")
+            menuFour.classList.toggle("show")
+            menuFive.classList.remove("show")
+            menuSix.classList.remove("show")
+            menuSeven.classList.remove("show")}
+            nav4.addEventListener("click", menuFourShow)
+            const menuFiveShow = ()=> {
+                menuOne.classList.remove("show")
+                menuTwo.classList.remove("show")
+                menuThree.classList.remove("show")
+                menuFour.classList.remove("show")
+                menuFive.classList.toggle("show")
+                menuSix.classList.remove("show")
+                menuSeven.classList.remove("show")}
+                nav5.addEventListener("click", menuFiveShow)
+            const menuSixShow = ()=>{
+                menuOne.classList.remove("show")
+                menuTwo.classList.remove("show")
+                menuThree.classList.remove("show")
+                menuFour.classList.remove("show")
+                menuFive.classList.remove("show")
+                menuSix.classList.toggle("show")
+                menuSeven.classList.remove("show")}
+                nav6.addEventListener("click", menuSixShow)
+                const menuSevenShow = ()=>{
+                    menuOne.classList.remove("show")
+                    menuTwo.classList.remove("show")
+                    menuThree.classList.remove("show")
+                    menuFour.classList.remove("show")
+                    menuFive.classList.remove("show")
+                    menuSix.classList.remove("show")
+                    menuSeven.classList.toggle("show")}
+                    nav7.addEventListener("click", menuSevenShow)
+
+    
+
+
+
+
+
